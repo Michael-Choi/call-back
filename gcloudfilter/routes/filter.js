4d27d4eb9828a22b935c5e34067cd5aced82436f
Michael-Choi <Michael.choi@queensu.ca>
2019-10-20 11:08:27 -0400

    fix google cloud route

--- gcloudfilter/routes/filter.js
+++ gcloudfilter/routes/filter.js
@@ -8,11 +8,16 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
 //we receive information through a post request to localhost:8000/rawdata then we run the google classification function on each input decide if its relevant and make a post request to the genesys api
-router.post("/rawdata", (req, res) => {
+router.post("/rawdata", async (req, res) => {
+  let arr = [];
   for (let qna of req.body) {
-    let category = classifyText(qna.faq.question, qna.faq.answer);
+    let category = await classifyText(qna.faq.question, qna.faq.answer);
+    console.log("Question: ", qna.faq.question);
+    // console.log("Answer: ", qna.faq.answer);
     console.log(category);
+    arr.push(category);
   }
+  res.json(arr).sendStatus(200);
 });
 
 module.exports = router;
--- gcloudfilter/server.js
+++ gcloudfilter/server.js
@@ -2,15 +2,14 @@
 
 const express = require("express");
 const filter = require("./routes/filter");
-
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
 
 //all routes that start with /urls get handled here
 
-// app.use("/", home);
+app.use("/", filter);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
